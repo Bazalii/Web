f119c8c48c3d993a552f6c8004fbd5bc46df5a54
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-11-05 16:48:30 +0300

    feat: realise navigationLocation
    - Realise function handleLocationChange

---
+++ js/navigationLocation.js
@@ -0,0 +1,22 @@
+function handleLocationChange(location, background, color) {
+    let menu = document.querySelectorAll('body header nav a')
+
+    switch (location) {
+        case '/Web/index.html':
+            menu[0].style.background = background
+            menu[0].style.color = color
+            break
+        case '/Web/Achievements.html':
+            menu[1].style.background = background
+            menu[1].style.color = color
+            break
+        case '/Web/Projects.html':
+            menu[2].style.background = background
+            menu[2].style.color = color
+            break
+        case '/Web/WorkExperience.html':
+            menu[3].style.background = background
+            menu[3].style.color = color
+            break
+    }
+}
