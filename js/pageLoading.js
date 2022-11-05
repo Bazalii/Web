a3413d93d017c9e9ee10802617633b889bfa6121
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-11-05 17:44:14 +0300

    feat: realise pageLoading
    - Realise methods: startLoadingTimeMeasurement, calculateLoadingTime
      and showLoadingTime

---
+++ js/pageLoading.js
@@ -0,0 +1,16 @@
+let loadingTime = 0
+
+function startLoadingTimeMeasurement() {
+    loadingTime = Date.now()
+}
+
+function calculateLoadingTime() {
+    return (Date.now() - loadingTime) / 1000 + ' seconds'
+}
+
+function showLoadingTime() {
+    let element = document.getElementById("loadingInformation");
+    let text = document.createTextNode(calculateLoadingTime());
+
+    element.appendChild(text);
+}
