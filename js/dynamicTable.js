57160a83a7778135ee076c40f7f9c758df66fa98
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-12-05 10:24:35 +0300

    feat: add validations for columnsNumber and rowsNumber

--- js/dynamicTable.js
+++ js/dynamicTable.js
@@ -38,6 +38,14 @@ function setOnClickHandlers() {
 }
 
 function createTable(parent, columnsNumber, rowsNumber) {
+    if (columnsNumber < 1) {
+        throw new Error('Columns number cannot be less than 1!');
+    }
+
+    if (rowsNumber < 1) {
+        throw new Error('Rows number cannot be less than 1!');
+    }
+
     let table = document.createElement('table');
 
     for (let i = 0; i < rowsNumber; i++) {
